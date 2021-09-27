b77ae41febca9790f60eb8c27336c013e538d205
Aleix Cortines <aleix@insiktintelligence.com>
2021-09-27 22:46:20 +0200

    logic of the game done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import "./App.css";
 import Board from "./components/Board";
 
 const App = () => {
+  const winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   const [turn, setTurn] = useState("X");
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [score, setScore] = useState({
     X: 0,
     O: 0,
   });
-  const isWinner = (squares) => {
+  const isWinner = (newSquares) => {
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [a, b, c] = winningPatterns[i];
+      if (
+        newSquares[a] &&
+        newSquares[a] === newSquares[b] &&
+        newSquares[a] === newSquares[c]
+      ) {
+        endGame(newSquares[a], winningPatterns[i]);
+        return;
+      }
+      if (!newSquares.includes(null)) {
+        endGame(null, Array.from(Array(10).keys()));
+        return;
+      }
+    }
     setTurn(turn === "X" ? "O" : "X");
   };
   const handleClick = (square) => {
@@ -17,6 +42,19 @@ const App = () => {
     newSquares.splice(square, 1, turn);
     setSquares(newSquares);
     isWinner(newSquares);
+  };
+
+  const endGame = (result) => {
+    setTurn(null);
+    if (result !== null) {
+      setScore({
+        ...score,
+        [result]: score[result] + 1,
+      });
+      alert(`Congratulations!! ${result} wins!!`);
+    } else {
+      alert("Try other time, push button");
+    }
   };
   return (
     <div className="container">
